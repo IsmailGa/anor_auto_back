e37ed5ecbfa4013e9c2ec0b25259b33284fa4123
Ismail <gaismail777@gmail.com>
2024-12-15 17:17:14 +0500

    Deploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser")
 
 const app = express();
 app.use(cors({
-    origin: process.env.FRONT_DOMAIN,
+    origin: [process.env.FRONT_DOMAIN, "http://5.182.26.85:3000"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }));
